5008c0f98fcad28e0604ab8204766c6b5308c489
GuptaVaishali <vaishaligupta982@gmail.com>
2021-09-22 18:25:11 +0530

    Unique element in array using sort

---
+++ Ques6.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ques6 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of array");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        int unique_element = -1;
+        boolean isThere = false;
+
+        for(int i=0;i<arr.length-1;i+=2){
+            if(arr[i] != arr[i+1]){
+                unique_element = arr[i];
+                isThere = true;
+                break;
+            }
+        }
+        if(!isThere)
+            unique_element = arr[arr.length -1];
+
+        if(unique_element != -1)
+            System.out.println("Unique Element is = " + unique_element);
+        else
+            System.out.println("Unique Element is not present in  array");
+    }
+}
